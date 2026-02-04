269bc184e5e9408daca39935719eabf9095be774
Yashwanth137 <Yashwanth137@users.noreply.github.com>
2026-02-05 02:04:38 +0530

    chore: update CORS settings for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+   # allow_origins=["http://localhost:3000"], -> for local development
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
